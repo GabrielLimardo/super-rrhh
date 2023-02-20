5885348e251f1045c92ae495b63771c80c2a0720
GabrielLimardo <gabriellim3001@gmail.com>
2023-02-20 11:41:51 -0300

    routes coment delete

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,6 @@ use App\Http\Controllers\VisualController;
 use App\Http\Controllers\DocumentTypeController;
 use App\Http\Controllers\CertificadoController;
 
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('auth.login');
 });
